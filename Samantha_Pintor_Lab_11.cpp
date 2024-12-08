d0ebf381636aac04a69d643159c6451678e118c4
Samanthapintor <samanthap329@nycstudents.net>
2024-12-08 14:33:39 -0500

    Update Samantha_Pintor_Lab_11.cpp

--- Samantha_Pintor_Lab_11.cpp
+++ Samantha_Pintor_Lab_11.cpp
@@ -1,6 +1,6 @@
 //Samantha Pintor 
-//Lab 4 
-//11-12-24
+//Lab 11 Arrays 
+//12-08-24
 
 #include <iostream> 
 #include <cmath> 
@@ -11,63 +11,65 @@ using namespace std;
 
 int main ()
 {
-    const int ARRAY_SIZE = 50; //list array of size 50
+    const int ARRAY_SIZE = 50; //List array size as 50
     double alpha[ARRAY_SIZE];
     
-    //declaring variables
-    int index; //variable index 
-    char space = ' '; //character called space
-    double sum; //variable for sum
-    int equalToHundred; //variable for exactly equal to one hundred 
-
-    srand(time(0)); //each time the program runs a unique sequence is generated
-
+    //Declaring variables
+    int index; //Variable for index
+    double sum = 0; //Initializes variable sum to 0
+    int equalToHundred = 0; //Initializes variable equalToHundred to 0
     
-    for (int index = 0; index < 25; index ++) //loop - increments until equal to 25
-        {
-        cout << pow(index,2) << space; //finding square root of x
+    srand(time(0)); //Each time the program runs a unique sequence is generated
+
+    //Part 1
+    //First loop: Print squares of numbers from 0 to 24
+    for (index = 0; index < 25; index++) {
+        cout << pow(index, 2) << " "; //Squares the numbers
         
-        if ((index+1) % 10 == 0 && index > 0){
-            cout << endl;}
+        if ((index + 1) % 10 == 0) { //Loop starts from 0, checks if (index + 1) is divisible by 10
+            cout << endl; //Insert newline every 10 numbers
         }
-    
-    for (int index = 25; index < 50; index ++) //loop - increments until equal to 25
-        {
-        cout << index * 3 << space; //finding square root of x
+    }
+
+    //Second loop: Multiply the index values from 25 to 49 by 3 
+    for (index = 25; index < 50; index++) {
+        cout << index * 3 << " "; //Multiples the numbers by 3 
         
-        if ((index+1) % 10 == 0 && index > 25){
-            cout << endl;}
+        if ((index + 1) % 10 == 0) { //Loop starts from 0, checks if (index + 1) is divisible by 10
+            cout << endl; //Insert newline every 10 numbers
         }
-    /*
-    0 1 4 9 16 25 36 49 64 81 
-    100 121 144 169 196 225 256 289 324 361 
-    400 441 484 529 576 75 78 81 84 87 
-    90 93 96 99 102 105 108 111 114 117 
-    120 123 126 129 132 135 138 141 144 147
-    */
+    }
 
-
-    for (index == 0; index < 50; index ++) //there are only be 50 randomly generated numbers
-    {
-        alpha[index] = rand() % 100 + 1; //index is random numbers between 1 and 100
-        cout << alpha[index] << space; //generating the list 
-        sum = sum + alpha[index]; //finding the sum 
-    
-        if (alpha[index] % 100 == 0){ //if the number divided by 100 is equal to 0
-            equalToHundred + 1; //there is another number exactly equal to 100
+    //Part 2
+    //Generate 50 random numbers, calculate sum and state numbers equal to 100
+    for (index = 0; index < 50; index++) {
+        alpha[index] = rand() % 100 + 1; //Generate random numbers between 1 and 100
+        cout << alpha[index] << " "; //Output the generated number
+        sum += alpha[index]; //Add the number to sum
+        
+        if (alpha[index] == 100) { //If the number is exactly 100
+            equalToHundred++; //Increment
         }
-
     }
     
-    //statements 
-    cout << endl << "The average is..." << (sum/50) << endl; 
+    //Output the average and total numbers equal to 100
+    cout << endl << "The average is... " << (sum / 50) << endl; 
     cout << "There are " << equalToHundred << " numbers equal to one hundred." << endl;
-
-    /*
-    17 43 81 71 62 100 39 38 67 38 31 30 52 82 55 34 85 48 4 70 89 56 98 95 82 72 51 57 49 68 23 17 62 3 88 23 3 78 60 21 15 42 2 67 23 56 100 8 4 55 
-    The average is...50.28
-    There are 0 numbers equal to one hundred.
-    */
 
     return 0; 
 }
+
+/* Output: 
+
+Part 1: 
+0 1 4 9 16 25 36 49 64 81 
+100 121 144 169 196 225 256 289 324 361 
+400 441 484 529 576 75 78 81 84 87 
+90 93 96 99 102 105 108 111 114 117 
+120 123 126 129 132 135 138 141 144 147 
+
+Part 2: 
+15 51 79 90 93 49 89 2 19 99 43 54 40 99 75 60 57 23 82 91 1 45 9 48 14 69 78 99 42 61 30 56 12 60 45 56 9 86 57 79 36 51 84 75 2 10 34 58 33 67 
+The average is... 52.32
+There are 0 numbers equal to one hundred.
+*/
